c03470f833e3469d25010bf90297a9f48a7ca9c2
synapticloop <contact@synapticloop.com>
2024-04-05 14:03:37 +1100

    updated to new namespace h2zero re-gen

---
+++ src/main/java/com/synapticloop/h2zero/extension/TaglibExtension.java
@@ -0,0 +1,174 @@
+package com.synapticloop.h2zero.extension;
+
+/*
+ * Copyright (c) 2018 synapticloop.
+ * 
+ * All rights reserved.
+ *
+ * This source code and any derived binaries are covered by the terms and
+ * conditions of the Licence agreement ("the Licence").  You may not use this
+ * source code or any derived binaries except in compliance with the Licence.
+ * A copy of the Licence is available in the file named LICENCE shipped with
+ * this source code or binaries.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * Licence for the specific language governing permissions and limitations
+ * under the Licence.
+ */
+
+import java.io.File;
+import java.util.*;
+
+import org.apache.commons.collections.IteratorUtils;
+import org.json.JSONObject;
+
+import com.synapticloop.h2zero.extension.Extension;
+import com.synapticloop.h2zero.model.Counter;
+import com.synapticloop.h2zero.model.Database;
+import com.synapticloop.h2zero.model.Finder;
+import com.synapticloop.h2zero.model.Options;
+import com.synapticloop.h2zero.model.Question;
+import com.synapticloop.h2zero.model.Table;
+import com.synapticloop.h2zero.model.View;
+import com.synapticloop.h2zero.validator.BaseValidator;
+import com.synapticloop.templar.Parser;
+import com.synapticloop.templar.exception.ParseException;
+import com.synapticloop.templar.exception.RenderException;
+import com.synapticloop.templar.utils.TemplarContext;
+
+/**
+/**
+ * This generator will generate all java classes for the tag libraries and the tag
+ * library descriptor file
+ * 
+ * <ul>
+ *   <li>Finders</li>
+ *   <li>Counters</li>
+ *   <li>Questions</li>
+ *   <li>FindByPrimaryKey</li>
+ *   <li>FindAll</li>
+ *   <li>CountAll</li>
+ * </ul>
+ * 
+ * @author synapticloop
+ */
+
+public class TaglibExtension extends Extension {
+	public static final String KEY_WEBAPP_OUTPUT_DIR = "webappOutputDir";
+
+	private static final Map<String, String> REQUIRED_OPTIONS = new HashMap<>();
+	static {
+		REQUIRED_OPTIONS.put(KEY_WEBAPP_OUTPUT_DIR, "Output directory for webapps - think 'src/main/webapp'");
+	}
+
+	@Override public String getRequiredOutputOptionDescription(String optionName) {
+		return(REQUIRED_OPTIONS.getOrDefault(KEY_WEBAPP_OUTPUT_DIR, "No description given."));
+	}
+
+	@Override public List<String> getRequiredOutputOptions() {
+		return(IteratorUtils.toList(REQUIRED_OPTIONS.keySet().iterator()));
+	}
+
+	@Override public Map<String, String> getDefaultOutputOptions() {
+		// TODO - static please
+		Map<String, String> defaultOptions = new HashMap<>();
+		defaultOptions.put("webappWebInfDir", "src/main/webapp");
+		return(defaultOptions);
+	}
+
+	@Override
+	public void generate(JSONObject extensionOptions, Database database, Options options, File outFile, boolean verbose) throws RenderException, ParseException {
+		// you __ALWAYS__ want to get the defaultTemplarContext.
+		TemplarContext templarContext = getDefaultTemplarContext(extensionOptions, database, options);
+
+		Parser javaCreateTaglibFinderParser = getParser("/java-create-taglib-finder.templar", verbose);
+		Parser javaCreateTaglibCounterParser = getParser("/java-create-taglib-counter.templar", verbose);
+		Parser javaCreateTaglibQuestionParser = getParser("/java-create-taglib-question.templar", verbose);
+		Parser javaCreateTaglibFinderFindByPrimaryKeyParser = getParser("/java-create-taglib-finder-find-by-primary-key.templar", verbose);
+		Parser javaCreateTaglibFinderFindAllParser = getParser("/java-create-taglib-finder-find-all.templar", verbose);
+		Parser javaCreateTaglibCounterCountAllParser = getParser("/java-create-taglib-counter-count-all.templar", verbose);
+
+		// the TLD
+		Parser tldCreateLibraryParser = getParser("/tld-create-library.templar", verbose);
+
+		// now for the tables
+		List<Table> tables = database.getTables();
+
+		for (Table table : tables) {
+			templarContext.add("table", table);
+			logInfo("Generating for table '" + table.getName() + "'.");
+
+			List<Finder> finders = table.getFinders();
+
+			for (Finder finder : finders) {
+				templarContext.add("finder", finder);
+
+				String pathname = outFile.getAbsolutePath() + options.getOutputCode() + database.getPackagePath() + "/taglib/" + table.getJavaFieldName() + "/" + finder.getTagName() + "Tag.java";
+				renderToFile(templarContext, javaCreateTaglibFinderParser, pathname, verbose);
+			}
+
+			List<Counter> counters = table.getCounters();
+
+			for (Counter counter : counters) {
+				templarContext.add("counter", counter);
+				String pathname = outFile.getAbsolutePath() + options.getOutputCode() + database.getPackagePath() + "/taglib/" + table.getJavaFieldName() + "/" + counter.getTagName() + "Tag.java";
+				renderToFile(templarContext, javaCreateTaglibCounterParser, pathname, verbose);
+			}
+
+			List<Question> questions = table.getQuestions();
+
+			for (Question question : questions) {
+				templarContext.add("question", question);
+				String pathname = outFile.getAbsolutePath() + options.getOutputCode() + database.getPackagePath() + "/taglib/" + table.getJavaFieldName() + "/" + question.getTagName() + "Tag.java";
+				renderToFile(templarContext, javaCreateTaglibQuestionParser, pathname, verbose);
+			}
+
+
+			// the extra 'missing' finders
+			String pathname = outFile.getAbsolutePath() + options.getOutputCode() + database.getPackagePath() + "/taglib/" + table.getJavaFieldName() + "/" + "FindByPrimaryKeyTag.java";
+			renderToFile(templarContext, javaCreateTaglibFinderFindByPrimaryKeyParser, pathname, verbose);
+
+			pathname = outFile.getAbsolutePath() + options.getOutputCode() + database.getPackagePath() + "/taglib/" + table.getJavaFieldName() + "/FindAllTag.java";
+			renderToFile(templarContext, javaCreateTaglibFinderFindAllParser, pathname, verbose);
+
+			pathname = outFile.getAbsolutePath() + options.getOutputCode() + database.getPackagePath() + "/taglib/" + table.getJavaFieldName() + "/CountAllTag.java";
+			renderToFile(templarContext, javaCreateTaglibCounterCountAllParser, pathname, verbose);
+		}
+
+		// now for the views
+		List<View> views = database.getViews();
+		for (View view : views) {
+			templarContext.add("view", view);
+			// hack for finder taglibs for views - should be split out
+			templarContext.add("table", view);
+
+			List<Finder> finders = view.getFinders();
+
+			for (Finder finder : finders) {
+				templarContext.add("finder", finder);
+
+				String pathname = outFile.getAbsolutePath() + options.getOutputCode() + database.getPackagePath() + "/taglib/" + view.getJavaFieldName() + "/" + finder.getTagName() + "Tag.java";
+				renderToFile(templarContext, javaCreateTaglibFinderParser, pathname, verbose);
+
+				pathname = outFile.getAbsolutePath() + options.getOutputCode() + database.getPackagePath() + "/taglib/" + view.getJavaFieldName() + "/FindAllTag.java";
+				renderToFile(templarContext, javaCreateTaglibFinderFindAllParser, pathname, verbose);
+
+				pathname = outFile.getAbsolutePath() + options.getOutputCode() + database.getPackagePath() + "/taglib/" + view.getJavaFieldName() + "/CountAllTag.java";
+				renderToFile(templarContext, javaCreateTaglibCounterCountAllParser, pathname, verbose);
+
+			}
+		}
+
+
+		// the tlds - for each database type
+		String pathname = outFile.getAbsolutePath() + "/" + extensionOptions.getString(KEY_WEBAPP_OUTPUT_DIR)  + "/WEB-INF/tld/" + database.getSchema() + "." + options.getDatabase() +".tld";
+		renderToFile(templarContext, tldCreateLibraryParser, pathname, verbose);
+	}
+
+	@Override
+	public List<BaseValidator> getValidators() {
+		return null;
+	}
+}
---
+++ src/main/java/com/synapticloop/h2zero/extension/taglib/BaseVarTag.java
@@ -0,0 +1,116 @@
+package com.synapticloop.h2zero.extension.taglib;
+
+/*
+ * Copyright (c) 2012-2018 synapticloop.
+ * All rights reserved.
+ *
+ * This source code and any derived binaries are covered by the terms and
+ * conditions of the Licence agreement ("the Licence").  You may not use this
+ * source code or any derived binaries except in compliance with the Licence.
+ * A copy of the Licence is available in the file named LICENCE shipped with
+ * this source code or binaries.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the Licence is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * Licence for the specific language governing permissions and limitations
+ * under the Licence.
+ */
+
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.PageContext;
+import jakarta.servlet.jsp.tagext.BodyTagSupport;
+
+import java.io.Serial;
+
+public class BaseVarTag extends BodyTagSupport {
+	@Serial private static final long serialVersionUID = 4908196758573941815L;
+
+	protected String var = null;
+	protected Integer offset = 0;
+	protected Integer limit = null;
+
+	protected boolean removeVar = false;
+
+	@Override
+	public int doEndTag() throws JspException {
+		if(removeVar) {
+			pageContext.removeAttribute(var, PageContext.PAGE_SCOPE);
+		}
+
+		this.limit = null;
+		this.offset = 0;
+
+		return(EVAL_PAGE);
+	}
+
+	/**
+	 * Get the name of the variable that will be placed in the PageContext.PAGE_SCOPE
+	 * 
+	 * @return the name of the variable
+	 */
+	public String getVar() { return var; }
+	
+	/**
+	 * Set the name of the variable that will be placed in the PageContext.PAGE_SCOPE
+	 * @param var
+	 */
+	public void setVar(String var) { this.var = var; }
+	
+	/**
+	 * Set whether to remove the variable after the end tag (otherwise it will
+	 * remain in the PageContext.PAGE_SCOPE)
+	 * 
+	 * @param removeVar whether to remove the variable at the end of the scope
+	 */
+	public void setRemoveVar(boolean removeVar) { this.removeVar = removeVar; }
+	
+	/**
+	 * Get whether to remove the variable from the PageContext.PAGE_SCOPE.
+	 * 
+	 * @return whether to remove the variable from the PageContext.PAGE_SCOPE at the end of the tag
+	 */
+	public boolean getRemoveVar() { return removeVar; }
+
+	/**
+	 * Set the offset for the results from the SQL query
+	 * 
+	 * @return the offset for results for the SQL query
+	 */
+	public String getOffset() { if(null != this.offset) { return(this.offset.toString()); } else { return(null); } }
+	
+	/**
+	 * set the offset for the results of the SQL query
+	 * 
+	 * @param offset the offset for results for the SQL query
+	 */
+	public void setOffset(String offset) {
+		try {
+			this.offset = Integer.valueOf(offset);
+		} catch(NumberFormatException ex) {
+			// TODO
+			// throw new Exception("Could not convert " + offset + " to an integer.");
+		}
+	}
+
+	/**
+	 * Get the limit (i.e. the number of results to be returned from the SQL query)
+	 * 
+	 * @return the limit of results for the end of the query
+	 */
+	public String getLimit() { if(null != this.limit) { return(this.limit.toString()); } else { return(null); } }
+
+	/**
+	 * Set the limit for the number of queries
+	 * 
+	 * @param limit the limit of the number of results for the query
+	 */
+	public void setLimit(String limit) {
+		try {
+			this.limit = Integer.valueOf(limit);
+		} catch(NumberFormatException ex) {
+			// do nothing
+		}
+	}
+
+}
